497dd7270eb3327faf17f129481f81796e1ffbce
Bfirstzy <bfirst152678@gmail.com>
2024-10-08 13:43:55 +0700

    Create calculator.py

---
+++ cell04/ex4.2/calculator.py
@@ -0,0 +1,12 @@
+F_num = int(input("Give me the first number: "))
+S_num = int(input("Give me the second number: "))
+print("Thank you!")
+add = F_num + S_num
+sub = F_num - S_num
+div = F_num / S_num
+mul = F_num * S_num
+
+print(str(F_num) + " + " + str(S_num) + " = " + str(add))
+print(str(F_num) + " - " + str(S_num) + " = " + str(sub))
+print(str(F_num) + " / " + str(S_num) + " = " + str(div))
+print(str(F_num) + " * " + str(S_num) + " = " + str(mul))
