5c42ef6b7c3e3c151c61e5c548439232ce54ea79
Bfirstzy <bfirst152678@gmail.com>
2024-10-08 10:44:50 +0700

    Delete cell03/to25.py

--- cell03/to25.py
+++
@@ -1,8 +0,0 @@
-print("Enter a number less than 25")
-I = int(input(" "))
-if I <= 25:
-    while I < 25:
-        I +=1
-        print("Inside the loop, my variable is"+ " " + str(I))
-else:
-    print("Error")
