75fc32d430dcc896359b1e47cb1f6f96446de0f3
Bfirstzy <bfirst152678@gmail.com>
2024-10-08 09:42:17 +0700

    Add files via upload

---
+++ cell03/i_got_that.py
@@ -0,0 +1,10 @@
+conversation = input("What you gotta say? : ")
+ask = input("I got that! Anything else? : ")
+stop = "STOP"
+while ask != stop:
+    
+    ask= input("I got that! Anything else? :")
+    if ask == stop :
+        break
+
+    
